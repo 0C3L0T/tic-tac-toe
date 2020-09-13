194f1f120177762f52ff5f0451ea324a20a9341e
0C3L0T <m3r1jnl@gmail.com>
2020-09-13 21:36:55 +0200

    cleanup

--- src/dist/board.dev.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var board = [['', '', ''], ['', '', ''], ['', '', '']];
-var players = ['X', 'O']; //draw board from state
-//make clickable, start with X
-//save state
-//hand state off to logic
-//fill board with input from logic
-//draw winner (maybe some cool screen or smth?)
--- src/dist/players.dev.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-
-var players = ['X', 'O']; //choose random player
